ee19eec6da8415212bd4c0a39ca595d3a307f832
xintao <973169778@qq.com>
2022-07-09 20:18:58 +0800

    first commit

---
+++ src/main/java/com/w/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.w.git;
+
+/**
+ * @author xin
+ * @date 2022-0709-20:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
