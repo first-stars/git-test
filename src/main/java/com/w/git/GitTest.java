85e5e011c289913e808968ec8050da619bd5974d
xintao <973169778@qq.com>
2022-07-09 20:21:08 +0800

    second commit

--- src/main/java/com/w/git/GitTest.java
+++ src/main/java/com/w/git/GitTest.java
@@ -2,10 +2,10 @@ package com.w.git;
 
 /**
  * @author xin
- * @date 2022-0709-20:09
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
